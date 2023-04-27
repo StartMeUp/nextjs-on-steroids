696858d2dcb4f292f2905e1c194fc4458877058a
StartMeUp <contact@start-me-up.fr>
2023-04-27 17:44:05 +0200

    feat: set up default tailwind breakpoints in storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,30 @@
 import type { Preview } from "@storybook/react";
 import "../src/styles/globals.css";
 
+const tailwindBreakpoints = {
+  sm: 640,
+  md: 768,
+  lg: 1024,
+  xl: 1280,
+  "2xl": 1536,
+};
+
+const tailwindViewPorts = Object.fromEntries(
+  Object.entries(tailwindBreakpoints).map(([key, val], idx) => {
+    console.log(val);
+    return [
+      key,
+      {
+        name: key,
+        styles: {
+          width: `${val}px`,
+          height: `${(idx + 5) * 10}vh`,
+        },
+      },
+    ];
+  })
+);
+
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
@@ -10,6 +34,7 @@ const preview: Preview = {
         date: /Date$/,
       },
     },
+    viewport: { viewports: tailwindViewPorts },
   },
 };
 
