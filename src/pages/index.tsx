4118c67ec485b1e53c5b9582cd7edcd240b9a7df
StartMeUp <contact@start-me-up.fr>
2023-04-28 22:40:02 +0200

    build: add @t3-oss/env-nextjs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,12 @@ import { env } from '@/env.mjs'
 import { log } from '@/services/logger'
 
 export default function Home() {
-  log.info('this is an info')
-  log.trace('this is a trace')
-  log.error('this an error')
-  log.warn('this is a warning')
-  log.debug('this is a debug')
-  log.fatal('this a fatal log')
-  console.log('log level', env.NEXT_PUBLIC_MINIMUM_LOG_LEVEL)
+  log.info('INFO', 'this is an info')
+  log.trace('TRACE', 'this is a trace')
+  log.error('ERROR', 'this an error')
+  log.warn('WARNING', 'this is a warning')
+  log.debug('DEBUG', 'this is a debug')
+  log.fatal('FATAL', 'this a fatal log')
+  console.log('log level =>', env.NEXT_PUBLIC_MINIMUM_LOG_LEVEL)
   return <h1 className="text-red-600 text-2xl">Home page</h1>
 }
--- src/services/logger.ts
+++ src/services/logger.ts
@@ -2,7 +2,7 @@ import pino, { Level } from 'pino'
 
 // you might want to display logs in production
 // adjust the level of log display
-const shouldDisplayLogs = !process.env.IS_PROD
+const shouldDisplayLogs = !(process.env.NODE_ENV === 'production')
 
 const logger = pino({
   level: process.env.NEXT_PUBLIC_MINIMUM_LOG_LEVEL || 'trace',
@@ -15,8 +15,8 @@ const logger = pino({
   }
 })
 
-const displayLog = (level: Level) => (msg: string) => {
-  if (shouldDisplayLogs) logger[level](msg)
+const displayLog = (level: Level) => (title: string, msg: string) => {
+  if (shouldDisplayLogs) logger[level](title + ' => ' + msg)
 }
 
 export const log = {
