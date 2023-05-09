7f21323f6e51a12d2f0f81020655a9110c23942b
StartMeUp <contact@start-me-up.fr>
2023-05-09 10:24:41 +0200

    build: rework todo button whit class-variance-authority

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -6,7 +6,7 @@ module.exports = {
 
   // Lint & Prettify TS and JS files
   '**/*.(ts|tsx|js)': (filenames) => [
-    `next lint --fix --file ${filenames
+    `yarn lint --fix --file ${filenames
       .map((f) => path.relative(process.cwd(), f))
       .join(' --file ')}`,
     `yarn prettier --write ${filenames.join(' ')}`
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
-import { useRef } from 'react'
-import type { AppProps } from 'next/app'
+import '@/assets/styles/globals.css'
 
 import {
   Hydrate,
@@ -7,8 +6,8 @@ import {
   QueryClientProvider
 } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-
-import '@/assets/styles/globals.css'
+import type { AppProps } from 'next/app'
+import { useRef } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = useRef(
